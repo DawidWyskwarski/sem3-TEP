f8d81b2802770768788918f29b47996bc86cfac8
DawidWyskwarski <153649273+DawidWyskwarski@users.noreply.github.com>
2024-10-22 19:20:31 +0200

    Added a modification

--- laby1/Ex2-3.cpp
+++ laby1/Ex2-3.cpp
@@ -33,11 +33,56 @@ bool deallocTable2dim(int ***table,int sizeX/*, int sizeY*/) {
     return true;
 }
 
+void fillTable(int*** table,int sizeX,int sizeY) {
+    for(int i = 0; i < sizeX; i++) {
+        for(int j = 0; j < sizeY; j++) {
+            (*table)[i][j] = 20;
+        }
+    }
+}
+
+void printTable(int**table,int sizeX,int sizeY) {
+    for(int i = 0; i < sizeX; i++) {
+        cout<<"[ ";
+        for(int j = 0; j < sizeY; j++) {
+            cout<< table[i][j]<<" ";
+        }
+        cout<<"]"<<endl;
+    }
+}
+
+//Mod
+bool insertCols(int*** table, int index, int cols,int sizeX, int sizeY) {
+
+    if(*table == NULL || index<0 || index>sizeY || cols <0 || sizeX <0 || sizeY <0) {
+        return false;
+    }
+
+    int **newTable = NULL;
+    allocTable2Dim(&newTable, sizeX, sizeY + cols);
+
+    for(int i = 0; i < sizeX; i++) {
+        for(int j = 0; j < index; j++) {
+            newTable[i][j] = (*table)[i][j];
+
+        }
+
+        for(int j = index; j < sizeY; j++) {
+            newTable[i][j + cols] = (*table)[i][j];
+        }
+    }
+    deallocTable2dim(table, sizeX);
+
+    *table = newTable;
+
+    return true;
+}
+
 int main() {
 
     int **table;
 
-    cout << allocTable2Dim(&table,3,5)<<endl;
+    /*cout << allocTable2Dim(&table,3,5)<<endl;
     cout << allocTable2Dim(&table,5,5)<<endl;
 
     cout << deallocTable2dim(&table,3)<<endl;
@@ -45,6 +90,16 @@ int main() {
 
     cout << allocTable2Dim(&table,5,5)<<endl;
     cout << deallocTable2dim(&table,5)<<endl;
+    */
+
+    allocTable2Dim(&table, 10, 10);
+    fillTable(&table, 10, 10);
+    printTable(table, 10, 10);
+
+    cout<<endl;
+
+    insertCols(&table, 8, 2, 10, 10);
+    printTable(table, 10, 14);
 
     return 0;
 }
--- laby1/Table.h
+++ laby1/Table.h
@@ -1,5 +1,6 @@
 #ifndef TABLE_H
 #define TABLE_H
+
 #include<string>
 
 class Table {
