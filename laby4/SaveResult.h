4e8d176dbf6876bbe1f8f6e3b8502d6e74f4a267
DawidWyskwarski <153649273+DawidWyskwarski@users.noreply.github.com>
2024-12-05 18:49:03 +0100

    5

---
+++ laby4/SaveResult.h
@@ -0,0 +1,67 @@
+//
+// Created by wyskw on 05.12.2024.
+//
+
+#ifndef SAVERESULT_H
+#define SAVERESULT_H
+#include <cmath>
+#include <fstream>
+
+#include "Result.h"
+#include "Error.h"
+#include "Tree.h"
+
+template<typename T>
+class SaveResult {
+public:
+    SaveResult(const std::string& file);
+
+    void save(Result<T,Error>& result);
+
+private:
+    std::string file;
+};
+
+template<typename T>
+SaveResult<T>::SaveResult(const std::string &file):file(file) {};
+
+template<typename T>
+void SaveResult<T>::save(Result<T, Error> &result) {
+    std::ofstream outFile;;
+
+    outFile.open(file + ".txt");
+
+    if(outFile.is_open()) {
+        if(!result.isSuccess()) {
+            for(Error* error: result.getErrors()) {
+                outFile << error->getErrorMessage() << std::endl;
+            }
+        }
+    }
+    outFile.close();
+}
+
+template<>
+void SaveResult<Tree*>::save(Result<Tree*, Error> &result) {
+    std::ofstream outFile;;
+
+    outFile.open(file + ".txt");
+
+    if(outFile.is_open()) {
+        if(!result.isSuccess()) {
+
+            for(Error* error: result.getErrors()) {
+                outFile << error->getErrorMessage() << std::endl;
+            }
+        }else {
+            outFile << result.getValue()->printTree().getValue() << std::endl;
+        }
+    }
+    outFile.close();
+}
+
+
+
+
+
+#endif //SAVERESULT_H
