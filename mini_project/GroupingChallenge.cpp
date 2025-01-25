57a170afd58837d1c6533d4a0c6a9435b317e16e
DawidWyskwarski <153649273+DawidWyskwarski@users.noreply.github.com>
2025-01-25 16:03:30 +0100

    separate file for parameters

--- mini_project/GroupingChallenge.cpp
+++ mini_project/GroupingChallenge.cpp
@@ -1,37 +1,44 @@
 #include "GaussianGroupingEvaluatorFactory.h"
+#include "GeneticAlgorithm.h"
 #include "GroupingEvaluator.h"
 #include "Optimizer.h"
+#include "SimulationParams.h"
 
 using namespace NGroupingChallenge;
 
 int main()
 {
-	CGaussianGroupingEvaluatorFactory c_evaluator_factory(5, 100, 5);
+	CGaussianGroupingEvaluatorFactory c_evaluator_factory(NUMBER_OF_GROUPS, NUMBER_OF_POINTS, NUMBER_OF_MULTIVARIATE_GAUSSIAN_DISTRIBUTION);
 
 	c_evaluator_factory
-		.cAddDimension(-100, 100, 1.0, 1.0)
-		.cAddDimension(-100, 100, 1.0, 1.0)
-		.cAddDimension(-100, 100, 1.0, 1.0)
-		.cAddDimension(-100, 100, 1.0, 1.0)
-		.cAddDimension(-100, 100, 1.0, 1.0)
-		.cAddDimension(-100, 100, 1.0, 1.0)
-		.cAddDimension(-100, 100, 1.0, 1.0)
-		.cAddDimension(-100, 100, 1.0, 1.0)
-		.cAddDimension(-100, 100, 1.0, 1.0)
-		.cAddDimension(-100, 100, 1.0, 1.0);
+		.cAddDimension(MEAN_MIN, MEAN_MAX, 1.0, 1.0)
+		.cAddDimension(MEAN_MIN, MEAN_MAX, 1.0, 1.0)
+		.cAddDimension(MEAN_MIN, MEAN_MAX, 1.0, 1.0)
+		.cAddDimension(MEAN_MIN, MEAN_MAX, 1.0, 1.0)
+		.cAddDimension(MEAN_MIN, MEAN_MAX, 1.0, 1.0)
+		.cAddDimension(MEAN_MIN, MEAN_MAX, 1.0, 1.0)
+		.cAddDimension(MEAN_MIN, MEAN_MAX, 1.0, 1.0)
+		.cAddDimension(MEAN_MIN, MEAN_MAX, 1.0, 1.0)
+		.cAddDimension(MEAN_MIN, MEAN_MAX, 1.0, 1.0)
+		.cAddDimension(MEAN_MIN, MEAN_MAX, 1.0, 1.0);
 
 	CGroupingEvaluator* pc_evaluator = c_evaluator_factory.pcCreateEvaluator(0);
+
+	GeneticAlgorithm ga(POPULATION_SIZE,CROSSING_CHANCE,MUTATION_CHANCE,*pc_evaluator);
 
 	COptimizer c_optimizer(*pc_evaluator);
 
 	c_optimizer.vInitialize();
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < ITERATIONS; i++)
 	{
-		c_optimizer.vRunIteration();
+		ga.runIteration();
+		cout<< ga.getBestFitness()<<endl;;
 	}
+	c_optimizer.vRunIteration();
 
 	delete pc_evaluator;
+
 
 	return 0;
 }
